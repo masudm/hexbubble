3ea66391634a55a2a805b977d3e8b70e2b8801b3
Masud Mohammed <0pixelz0@gmail.com>
2017-09-28 09:51:00 +0100

    added a public stylesheet. added a login and a post structure with the correct styles. added some other base dependencies

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const mysql = require('mysql');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 //database connection
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
@@ -20,13 +21,14 @@ connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
     console.log('The solution is: ', results[0].solution);
 });
 
-connection.end();
+connection.end();*/
 
 //API routes
 //create an instance for routes
 var apiRoutes = express.Router();
 
-app.use('/', require('./routes/login'));
+app.use('/login', require('./routes/login'));
+app.use('/', require('./routes/feed'));
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
--- routes/feed.js
+++ routes/feed.js
@@ -2,7 +2,7 @@ var express = require('express');
 var apiRoutes = express.Router();
 
 apiRoutes.get('/', function(req, res) {
-	res.render("login");
+	res.render("feed");
 });
 
 module.exports = apiRoutes;
