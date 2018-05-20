bbc3d2779da9149e2a6011527fcef4f926c66699
Masud Mohammed <0pixelz0@gmail.com>
2018-05-20 16:14:59 +0100

    perf change

--- public/js/particles.js
+++ public/js/particles.js
@@ -26,7 +26,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 20,
+        "value": 25,
         "density": {
           "enable": true,
           "value_area": 800
