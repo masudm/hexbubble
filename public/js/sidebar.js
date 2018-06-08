0c0b34b0f4e24745112e92262a9e2f09eaad8e32
Masud Mohammed <0pixelz0@gmail.com>
2018-06-08 09:28:59 +0100

    add a manage button

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -3,6 +3,16 @@ $.get("/me/bubbles", function(data, status){
     data = data.data;
     for (bubble in data) {
         b = data[bubble];
-        $('#meIcon').after("<li class='elem bubbleIcon' id='bubbleIcon_" + b.bubbleId + "' onclick=\"window.location.href = '/feed/" + b.bubbleId + "'\">\n            <img src='/bubblePictures/" + b.bubblePicture + "'>\n            <div class='bubbleName'>" + b.bubbleName + "</div>\n        </li>");
+
+        manage = true;
+        containerClass = "";
+        manageDiv = "";
+
+        if (manage) {
+            containerClass = "noHeight";
+            manageDiv = "<div class='manageBubble'><span class='button'>Manage</span></div>";
+        }
+        
+        $('#meIcon').after("<li class='elem bubbleIcon "+containerClass+"' id='bubbleIcon_" + b.bubbleId + "' onclick=\"window.location.href = '/feed/" + b.bubbleId + "'\">\n            <img src='/bubblePictures/" + b.bubblePicture + "'>\n            <div class='bubbleName'>" + b.bubbleName + "</div>"+manageDiv+"\n        </li>");
     }
 });
