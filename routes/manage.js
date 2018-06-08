b47698e42fcd36fe5b1444927493e207300386bd
Masud Mohammed <0pixelz0@gmail.com>
2018-06-08 10:47:59 +0100

    add the routes

--- routes/manage.js
+++ routes/manage.js
@@ -6,16 +6,44 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var db = require('../helpers/db'); //a reference to the database functions so they can be used
 
 apiRoutes.get('/:id', function(req, res) {
-    //check for auth of user - if they are actually an admin
-    db.isAdmin(req.decoded.userId, req.params.id, function(err, data) {
-        let isAdmin = !!data[0].admin;
-
+    isAdmin(req.decoded.userId, req.params.id, function(isAdmin) {
         if (!isAdmin) {
             res.send('You are not an admin.');
         } else {
-            res.render('manage');
+            res.render('manage', {bubbleId: req.params.id});
         }
     });
 });
+
+apiRoutes.post('/changePassword', function(req, res) {
+    let bubbleId = req.body.bubbleId;
+    let password = req.body.password;
+
+    isAdmin(req.decoded.userId, bubbleId, function(isAdmin) {
+        if (!isAdmin) {
+            res.send('You are not an admin.');
+        } else {
+            db.changeBubblePassword(bubbleId, password, function(err, data) {
+                if (err) {
+                    return res.json({
+                        success: false,
+                        error: err
+                    });
+                }
+                res.json({
+                    success: true,
+                    data: json
+                });
+            });
+        }
+    });
+});
+
+function isAdmin(userId, bubbleId, callback) {
+//check for auth of user - if they are actually an admin
+    db.isAdmin(userId, bubbleId, function(err, data) {
+        return callback(!!data[0].admin);
+    });
+}
 
 module.exports = apiRoutes;
